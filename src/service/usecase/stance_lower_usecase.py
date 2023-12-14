08480d43902d1e3ec5a217f4c086be4c690c7dde
miu <38824651+miu200521358@users.noreply.github.com>
2023-12-14 16:22:11 +0900

    下半身中IK追加

--- src/service/usecase/stance_lower_usecase.py
+++ src/service/usecase/stance_lower_usecase.py
@@ -3,7 +3,7 @@ from math import radians
 from typing import Optional
 
 from mlib.core.logger import MLogger
-from mlib.core.math import MQuaternion, MVector3D
+from mlib.core.math import MVector3D
 from mlib.pmx.bone_setting import BoneFlg
 from mlib.pmx.pmx_collection import PmxModel
 from mlib.pmx.pmx_part import Bone, DisplaySlotReference, Ik, IkLink
@@ -113,8 +113,8 @@ class StanceLowerUsecase:
         )
 
         # 足の長さ＋下半身から足中心までの長さの比率
-        # lower_leg_ratio = lower_ratio * leg_ratio
-        lower_leg_ratio = (lower_ratio + leg_ratio) / 2
+        lower_leg_ratio = lower_ratio * leg_ratio
+        # lower_leg_ratio = (lower_ratio + leg_ratio) / 2
 
         for fidx, fno in enumerate(dest_initial_matrixes.indexes):
             logger.count(
@@ -131,6 +131,10 @@ class StanceLowerUsecase:
             lower_bf = dest_motion.bones[BoneNames.lower()][fno]
             lower_bf.register = True
             dest_motion.insert_bone_frame(lower_bf)
+
+            lower_ik_bf = dest_motion.bones[BoneNames.lower_ik("中")][fno]
+            lower_ik_bf.register = True
+            dest_motion.insert_bone_frame(lower_ik_bf)
 
             for direction in ("右", "左"):
                 leg_bf = dest_motion.bones[BoneNames.leg(direction)][fno]
@@ -162,8 +166,6 @@ class StanceLowerUsecase:
             )
 
             # --------------
-
-            lower_qqs: list[MQuaternion] = []
 
             for direction in ("右", "左"):
                 # 下半身先
@@ -205,17 +207,13 @@ class StanceLowerUsecase:
                 # ).save()
                 # # ■ --------------
 
-                # IK解決する
-                _, _, ik_qqs = dest_motion.bones.get_ik_rotation(
-                    fidx,
-                    fno,
-                    dest_model,
-                    dest_model.bones[BoneNames.lower_ik(direction)],
-                )
-
-                lower_qqs.append(
-                    ik_qqs[dest_model.bones[BoneNames.lower_tail(direction)].index]
-                )
+                # # IK解決する
+                # _, _, ik_qqs = dest_motion.bones.get_ik_rotation(
+                #     fidx,
+                #     fno,
+                #     dest_model,
+                #     dest_model.bones[BoneNames.lower_ik(direction)],
+                # )
 
                 # # ■ --------------
 
@@ -240,14 +238,48 @@ class StanceLowerUsecase:
                 # ).save()
                 # # ■ --------------
 
-            # 両足の中間を採用する
+            # IK解決をした上でグローバル位置を求める
+            lower_matrixes = dest_motion.animate_bone(
+                [fno], dest_model, [BoneNames.knee_tail("右"), BoneNames.knee_tail("左")]
+            )
+
+            # 下半身中IK
+            lower_center_ik_bf = dest_motion.bones[BoneNames.lower_ik("中")][fno]
+            # 両足の中間に向ける
+            lower_center_ik_bf.position = (
+                lower_matrixes[BoneNames.leg_tail("右"), fno].position
+                + lower_matrixes[BoneNames.leg_tail("左"), fno].position
+            ) / 2
+            dest_motion.insert_bone_frame(lower_center_ik_bf)
+
+            # IK解決する
+            _, _, ik_qqs = dest_motion.bones.get_ik_rotation(
+                fidx,
+                fno,
+                dest_model,
+                dest_model.bones[BoneNames.lower_ik("中")],
+            )
+
             lower_bf = dest_motion.bones[BoneNames.lower()][fno]
-            lower_bf.rotation = MQuaternion.slerp(lower_qqs[0], lower_qqs[1], 0.5)
+            lower_bf.rotation = ik_qqs[dest_model.bones[BoneNames.lower()].index]
             dest_motion.insert_bone_frame(lower_bf)
+
+            # ■ --------------
+            from datetime import datetime
+
+            from mlib.vmd.vmd_writer import VmdWriter
+
+            VmdWriter(
+                dest_motion,
+                f"E:/MMD/サイジング/足IK/IK_step/{datetime.now():%Y%m%d_%H%M%S_%f}_下半身_{fno:04d}.vmd",
+                model_name="Test Model",
+            ).save()
+            # ■ --------------
 
         # 終わったらIKボーンのキーフレを削除
         del dest_motion.bones[BoneNames.lower_ik("右")]
         del dest_motion.bones[BoneNames.lower_ik("左")]
+        del dest_motion.bones[BoneNames.lower_ik("中")]
         del dest_motion.bones[BoneNames.lower_tail("右")]
         del dest_motion.bones[BoneNames.lower_tail("左")]
         del dest_motion.bones[BoneNames.leg_tail("右")]
@@ -369,8 +401,39 @@ class StanceLowerUsecase:
 
             lower_ik_bone.ik = lower_ik
 
-        ik_model.bones[BoneNames.root()].parent_index = -1
+        # 下半身IK ---------------
+        lower_center_ik_bone = Bone(
+            index=lower_ik_bone.index + 1,
+            name=BoneNames.lower_ik("中"),
+        )
+        lower_center_ik_bone.position = MVector3D()
+        lower_center_ik_bone.is_system = True
+        lower_center_ik_bone.bone_flg |= (
+            BoneFlg.IS_IK
+            | BoneFlg.CAN_TRANSLATE
+            | BoneFlg.CAN_ROTATE
+            | BoneFlg.CAN_MANIPULATE
+            | BoneFlg.IS_VISIBLE
+        )
+        ik_model.insert_bone(lower_center_ik_bone)
+        sizing_display_slot.references.append(
+            DisplaySlotReference(display_index=lower_center_ik_bone.index)
+        )
 
+        # 下半身IKの構成
+        lower_center_ik = Ik()
+        lower_center_ik.bone_index = ik_model.bones[BoneNames.leg_center()].index
+        lower_center_ik.loop_count = 20
+        lower_center_ik.unit_rotation.degrees = MVector3D(180, 0, 0)
+
+        # 下半身
+        lower_center_ik_lower = IkLink()
+        lower_center_ik_lower.bone_index = ik_model.bones[BoneNames.lower()].index
+        lower_center_ik.links.append(lower_center_ik_lower)
+
+        lower_center_ik_bone.ik = lower_center_ik
+
+        # 親ボーンとかの設定 -----------
         for direction in ("右", "左"):
             lower_tail_bone = ik_model.bones[BoneNames.lower_tail(direction)]
             leg_tail_bone = ik_model.bones[BoneNames.leg_tail(direction)]
@@ -383,6 +446,9 @@ class StanceLowerUsecase:
             leg_tail_bone.tail_index = knee_tail_bone.index
             knee_tail_bone.parent_index = leg_tail_bone.index
             lower_ik_bone.parent_index = ik_model.bones[BoneNames.root()].index
+
+        ik_model.bones[BoneNames.root()].parent_index = -1
+        lower_center_ik_bone.parent_index = ik_model.bones[BoneNames.root()].index
 
         ik_model.setup()
 
